e74a237dd4d1051ee33e533703ff5f6ef817bbca
lingeswarandhanapal7-hub <lingeswarandhanapal7@gmail.com>
2026-07-27 11:02:05 +0530

    fix: resolve Express 5 path-to-regexp wildcard route syntax in server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -84,7 +84,7 @@ const __dirname = path.dirname(__filename);
 const distPath = path.join(__dirname, '../dist');
 
 app.use(express.static(distPath));
-app.get('*', (req, res, next) => {
+app.use((req, res, next) => {
   if (req.path.startsWith('/api')) return next();
   res.sendFile(path.join(distPath, 'index.html'));
 });
